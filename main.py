9294b23eeb08bd2cfb271ba0fe3804e5f9090f53
Melissa Tejada <mtejada073@soyudemedellin.edu.co>
2022-08-21 10:35:09 -0500

    Correccion de respuesta del api

--- main.py
+++ main.py
@@ -8,29 +8,23 @@ app = FastAPI()
 
 Instrumentator().instrument(app).expose(app)
 
-
-# setup loggers
 logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
-
-# get root logger
 logger = logging.getLogger(__name__)  # the __name__ resolve to "main" since we are at the root of the project. 
                                       # This will get the root logger since no logger in the configuration has this name.
 
 
 @app.get("/")
 def read_root():
-    response = infoUser()
-    return {"items": response }
+    return authUsers()
 
 @app.get("/authUsers/{internalId}")
 def read_item(internalId : str):
-    list=infoUser()
+    list=authUsers()
     for item in list:
-        print(item)
         if item["internalId"]==internalId:
             return item
 
-def infoUser():
+def authUsers():
     url='https://62fef1fea85c52ee483e83bb.mockapi.io/authUsers'
     response = requests.get(url, {}, timeout=5)
     return response.json()
